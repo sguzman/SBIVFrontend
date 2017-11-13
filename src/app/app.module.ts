ad7a3c9aaa3cdb65cb27bc4b6c06b44694bdbe7e
Salvador Guzman <guzmansalv@gmail.com>
2017-11-12 22:36:29 -0800

    added modules to module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
 import {MaterialModule} from '../module/material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MapsComponent } from './maps/maps.component';
+import { ChartsComponent } from './charts/charts.component';
+import { ListComponent } from './list/list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    MapsComponent,
+    ChartsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
