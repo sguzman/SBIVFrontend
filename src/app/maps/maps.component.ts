112d6e962f2ec5ebdf7fa8bc7a56b9fe9c31fd99
Salvador Guzman <guzmansalv@gmail.com>
2017-11-12 22:45:25 -0800

    added google maps

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-maps',
@@ -7,8 +9,24 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class MapsComponent implements OnInit {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
-  constructor() { }
+  constructor() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    const coords = {latitude: 34.4145788, longitude: -119.8617748};
+    const latLng = new google.maps.LatLng(coords.latitude, coords.longitude);
+    const mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
 
   ngOnInit() {
   }
