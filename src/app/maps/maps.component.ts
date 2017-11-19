950a7172a0012a25e8bf71ad8a77eba0b9f19915
Salvador Guzman <guzmansalv@gmail.com>
2017-11-19 07:49:40 -0800

    included tweetservice provider

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,13 +1,16 @@
 import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {Tweet} from '../interface/tweet.interface';
+import {TweetService} from '../tweet.service';
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
   styleUrls: ['./maps.component.css'],
-  encapsulation: ViewEncapsulation.None
+  providers: [TweetService]
 })
 export class MapsComponent implements OnInit {
-  constructor() {}
+  tweets: Tweet[] = [];
+  constructor(public tweet: TweetService) {}
 
   ngOnInit() {
   }
