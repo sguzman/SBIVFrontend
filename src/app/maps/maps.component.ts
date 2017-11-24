27c48941358147f5980c3ca06f7f75720edaff4d
Salvador Guzman <guzmansalv@gmail.com>
2017-11-24 07:40:33 -0800

    request twitter data and map it

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,6 +1,12 @@
-import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Tweet} from '../interface/tweet.interface';
 import {TweetService} from '../tweet.service';
+
+import {Subject} from 'rxjs/Subject';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/map';
+import {split} from 'ts-node/dist';
+import {TweetRaw} from '../interface/tweet-raw.interface';
 
 @Component({
   selector: 'app-maps',
@@ -10,7 +16,48 @@ import {TweetService} from '../tweet.service';
 })
 export class MapsComponent implements OnInit {
   tweets: Tweet[] = [];
-  constructor(public tweet: TweetService) {}
+
+  up$ = new Subject();
+  constructor(public tweet: TweetService) {
+    this.up$.flatMap(() => this.tweet.tweets$())
+      .map(s => s['tweets'])
+      .flatMap(s => s)
+      .subscribe(
+        (s: TweetRaw) => {
+        try {
+          const text: string = s['text'];
+          const text2: string = text.substring(0, text.lastIndexOf(' *'));
+          const loc = text2.substring(text2.lastIndexOf('*3') + 1);
+
+          const pair = loc.split(' ');
+          const lat = pair[0].substr(0, 2) + '.' + pair[0].substring(2);
+          const lng = pair[1].substr(0, 4) + '.' + pair[1].substring(4);
+
+          const latNum = parseFloat(lat);
+          const lngNum = parseFloat(lng);
+
+          const aTweet: Tweet = {
+            date: s.date,
+            id: s.id,
+            text: s.text,
+            lat: latNum,
+            lng: lngNum
+          };
+
+          console.log(aTweet);
+          this.tweets.push(aTweet);
+        } catch (e) {
+          console.error(e);
+        }
+      },
+      error => console.error(`:( ${error}`),
+      () => console.log('Finished')
+    );
+  }
+
+  public update() {
+    this.up$.next();
+  }
 
   ngOnInit() {
   }
